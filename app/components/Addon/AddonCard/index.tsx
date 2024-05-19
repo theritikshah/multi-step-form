9d3004508cd31b8cb258541382fc86e437ec92c5
Ritik <ritikraw@gmail.com>
2024-05-19 21:34:02 +0530

    add addon card component

---
+++ app/components/Addon/AddonCard/index.tsx
@@ -0,0 +1,40 @@
+type Props = {
+  isSelected: boolean;
+  name: string;
+  id: number;
+  description: string;
+  rate: number;
+  onChange: (id: number) => void;
+};
+
+const AddonCard = ({
+  isSelected = false,
+  id,
+  name,
+  description,
+  rate,
+  onChange,
+}: Props) => {
+  return (
+    <div
+      className={`flex cursor-pointer    items-center p-3 rounded-lg border border-light-gray gap-4 
+      ${isSelected ? "bg-magnolia border-marine-blue" : "bg-white"}`}
+      onClick={() => onChange(id)}
+    >
+      <div
+        className={`flex justify-center border border-light-gray items-center rounded-sm p-1 aspect-square 
+      ${isSelected ? "bg-purplish-blue border-transparent" : "bg-white "}
+      `}
+      >
+        <img src="/assests/images/icon-checkmark.svg" alt="" />
+      </div>
+      <div className="flex flex-col ">
+        <span className="text-sm font-medium text-marine-blue">{name}</span>
+        <span className="text-sm font-thin text-cool-gray">{description}</span>
+      </div>
+      <span className="text-sm ml-auto  text-marine-blue">{`+$${rate}/mo`}</span>
+    </div>
+  );
+};
+
+export default AddonCard;
