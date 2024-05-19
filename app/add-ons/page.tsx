83e73b4bcfc953b322bd508860415000fc11c185
Ritik <ritikraw@gmail.com>
2024-05-19 19:00:51 +0530

    setup addon page and addon component

---
+++ app/add-ons/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Addon from "../components/Addon";
+
+type Props = {};
+
+const AddOnsPage = (props: Props) => {
+  return <Addon />;
+};
+
+export default AddOnsPage;
---
+++ app/components/Addon/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type Props = {};
+
+const Addon = (props: Props) => {
+  return (
+    <div className="flex flex-col gap-10 max-md:gap-5 w-full ">
+      <header className="flex flex-col gap-1">
+        <h1>Pick Add-ons</h1>
+        <p className="sub-title">
+          Add-ons help enhance your gaming experience.
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default Addon;
