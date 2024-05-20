a8cead38828edf58406d8423e04230dfa7472b8b
Ritik <ritikraw@gmail.com>
2024-05-20 22:41:38 +0530

    setup useForm

--- app/components/Steps/index.tsx
+++ app/components/Steps/index.tsx
@@ -1,23 +1,59 @@
 "use client";
 
 import { AnimatePresence, motion } from "framer-motion";
-import { usePathname } from "next/navigation";
-import React, { ReactNode, createContext, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+
+import React, { ReactNode, createContext, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 type Props = {
   children: ReactNode;
 };
 
-export const PlanContext = createContext<any>(null);
+export type FormType = {
+  name: string;
+  email: string;
+  phoneNumber: string;
+  isYearly: boolean;
+  selectedPlan: { [key: string]: any };
+  selectedAddons: { [key: number]: AddonType };
+};
+
+type AddonType = {
+  id: number;
+  name: string;
+  description: string;
+  [key: string]: any;
+};
+
+export const FormHookContext = createContext<any>(null);
 
 const Steps = ({ children, ...props }: Props) => {
+  const formHook = useForm<FormType>({
+    mode: "onBlur",
+    defaultValues: {
+      name: "",
+      email: "",
+      phoneNumber: "",
+      isYearly: false,
+      selectedPlan: {},
+      selectedAddons: {},
+    },
+  });
+
+  const { handleSubmit } = formHook;
+
   const [isLoading, setIsLoading] = useState(false);
+
   const path = usePathname();
+  const router = useRouter();
+
   useEffect(() => {
     if (isLoading) {
       setIsLoading(false);
     }
   }, [path]);
+
   const handleNext = () => {
     setIsLoading(true);
     switch (path) {
@@ -39,22 +75,33 @@ const Steps = ({ children, ...props }: Props) => {
   };
 
   return (
-    <PlanContext.Provider value={{ isYearly, setIsYearly }}>
-      <section className="flex flex-col md:overflow-hidden   bg-white max-md:bg-magnolia max-md:mx-auto  p-20 max-md:p-0 max-md:w-full w-full h-full max-md:m-4  pt-10 pb-6   max-md:gap-3">
+    <FormHookContext.Provider value={{ formHook }}>
+      <section className="flex  flex-col md:overflow-hidden   bg-white max-md:bg-magnolia max-md:mx-auto  p-20 max-md:p-0 max-md:w-full w-full h-full max-md:m-4  pt-10 pb-6   gap-3">
         <AnimatePresence mode="wait">
           <motion.section
             initial={{ x: "100vw" }}
             animate={{ x: 0 }}
             exit={{ x: "-100vw" }}
-            transition={{ type: "just" }}
+            transition={{ type: "just", duration: 0.2 }}
             key={path}
             className="flex md:first:w-full w-full  max-md:mt-[-4.7rem]  max-md:m-5 max-md:mb-24 rounded-3xl bg-white max-md:p-5 max-md:py-10  max-md:w-[calc(100%-1.25rem*2)]"
           >
             {children}
           </motion.section>
         </AnimatePresence>
-        <footer className="flex  items-center max-md:fixed max-md:bottom-0 bg-white  max-md:left-0 max-md:w-full  max-md:p-5  md:mt-auto  justify-between ">
+        {!(path == "/summary/thank-you") && (
+          <footer className="flex  items-center max-md:fixed max-md:bottom-0 bg-white  max-md:left-0 max-md:w-full  max-md:p-5  md:mt-auto  justify-between ">
+            {!(path == "/your-info" || path.includes("thank-you")) && (
               <button className="text-cool-gray" onClick={() => router.back()}>
+                Go Back
+              </button>
+            )}
+            <button
+              className={`bg-marine-blue text-white p-2 px-6 rounded-lg max-md:min-w-24 ${
+                path == "/your-info" ? "ml-auto" : ""
+              } ${path == "/summary" ? "bg-purplish-blue" : ""}`}
+              onClick={handleSubmit(handleNext)}
+            >
               {isLoading ? (
                 <Spinner />
               ) : path == "/summary" ? (
@@ -62,14 +109,16 @@ const Steps = ({ children, ...props }: Props) => {
               ) : (
                 "Next"
               )}
-          </button>
-        </footer>
+            </button>
+          </footer>
+        )}
       </section>
-    </PlanContext.Provider>
+    </FormHookContext.Provider>
   );
 };
 
 export default Steps;
+
 export function Spinner() {
   return (
     <div className="inline-block w-4 h-4 border-2 border-t-2 border-gray-200 border-t-transparent rounded-full animate-spin"></div>
