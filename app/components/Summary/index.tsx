dc09829995cb19ea9f0b1ec55f86bd79496143c4
Ritik <ritikraw@gmail.com>
2024-05-20 22:44:47 +0530

    Render summary with updated values via useFrom hook

--- app/components/Summary/index.tsx
+++ app/components/Summary/index.tsx
@@ -1,19 +1,48 @@
-import React from "react";
+"use client";
+import React, { useContext } from "react";
+import { FormHookContext, FormType } from "../Steps";
+import { FREE_MONTHS } from "@/app/constants";
+import { getRate } from "@/app/utils";
+import Link from "next/link";
 
 type Props = {};
 
-const AddOnsList = ({ name, rate }: { name: string; rate: number }) => {
+const AddOnsList = ({
+  name,
+  rate,
+  isYearly,
+}: {
+  name: string;
+  rate: number;
+  isYearly: boolean;
+}) => {
+  const planRate = getRate(rate, isYearly);
+
   return (
     <div className="flex justify-between">
       <span className="text-sm text-cool-gray">{name}</span>
       <span className="text-sm font-semibold text-marine-blue">
-        +${rate}/mo
+        +${planRate}/{isYearly ? "yr" : "mo"}
       </span>
     </div>
   );
 };
 
 const Summary = (props: Props) => {
+  const { formHook } = useContext(FormHookContext);
+  const { getValues } = formHook;
+
+  const { selectedPlan, selectedAddons, isYearly }: FormType = getValues();
+
+  const planRate = (selectedPlan && getRate(selectedPlan.rate, isYearly)) || 0;
+  const addonsRate =
+    selectedAddons &&
+    Object.values(selectedAddons)
+      .map(({ rate }) => rate)
+      ?.reduce((acc: number, rate) => acc + getRate(rate, isYearly), 0);
+
+  const totalRate = planRate + addonsRate;
+
   return (
     <div className="flex flex-col gap-10 max-md:gap-5 w-full ">
       <header className="flex flex-col gap-1">
@@ -27,21 +56,35 @@ const Summary = (props: Props) => {
           <div className="flex justify-between items-center">
             <div className="flex flex-col">
               <span className="text-sm font-bold text-marine-blue">
-                Arcade (Monthly)
+                {selectedPlan.name} {isYearly ? "(Yearly)" : "(Monthly)"}
               </span>
-              <a className="text-sm underline text-cool-gray">Change</a>
+              <Link
+                href="/select-plan"
+                className="text-sm underline text-cool-gray"
+              >
+                Change
+              </Link>
             </div>
-            <span className="text-sm font-bold text-marine-blue">$9/mo</span>
+            <span className="text-sm font-bold text-marine-blue">
+              ${planRate}/{isYearly ? "yr" : "mo"}
+            </span>
           </div>
           <hr className="bg-cool-gray" />
-          <AddOnsList name="Online service" rate={1} />
-          <AddOnsList name="Larger storate" rate={2} />
+          {Object.values(selectedAddons).map((addon) => {
+            return (
+              <AddOnsList
+                name={addon.name}
+                rate={addon.rate}
+                isYearly={isYearly}
+              />
+            );
+          })}
         </div>
 
         <div className="flex justify-between px-3">
           <span className="text-sm text-cool-gray">Total (per month)</span>
           <span className="text-base md:text-lg text-purplish-blue font-bold">
-            +$120/mo
+            {isYearly ? `$${totalRate}/yr` : `+$${totalRate}/mo`}
           </span>
         </div>
       </section>
