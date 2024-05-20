dbc6a5643fdecadbed5fb086af418edab361ac63
Ritik <ritikraw@gmail.com>
2024-05-20 22:42:54 +0530

    handle input form via useForm hook

--- app/components/Info/index.tsx
+++ app/components/Info/index.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+"use client";
+
+import React, { useContext } from "react";
 import Input from "../common/Input";
 import Label from "../common/Label";
+import { FormHookContext } from "../Steps";
+import { Controller } from "react-hook-form";
 
 type Props = {};
 
 const Info = (props: Props) => {
+  const { formHook } = useContext(FormHookContext);
+  const {
+    register,
+    formState: { errors },
+    trigger,
+    control,
+  } = formHook;
+
+  const ErrorMessage = ({ error }: { error: string }) => {
+    if (error) {
+      return (
+        <span className="font-semibold text-strawberry-red text-sm ml-auto">
+          {error}
+        </span>
+      );
+    }
+
+    return <></>;
+  };
+
   return (
     <div className="flex flex-col gap-10 max-md:gap-5 w-full ">
       <header className="flex flex-col gap-1">
@@ -15,23 +39,72 @@ const Info = (props: Props) => {
       </header>
       <form className="flex flex-col gap-4">
         <div className="flex flex-col gap-1">
-          <Label htmlFor="name">Name</Label>
-          <Input type="text" name="name" placeholder="e.g. Stephen King" />
-        </div>
-        <div className="flex flex-col gap-1">
-          <Label htmlFor="address">Address</Label>
-          <Input
-            type="text"
-            name="address"
-            placeholder="e.g. StephenKing@lorem.com"
+          <Label htmlFor="name">
+            Name
+            <ErrorMessage error={errors.name && errors.name.message} />
+          </Label>
+
+          <Controller
+            name="name"
+            control={control}
+            rules={{
+              required: "Name is required",
+            }}
+            render={({ field }) => (
+              <Input
+                {...field}
+                placeholder="e.g. Stephen King"
+                hasError={errors.name}
+              />
+            )}
           />
         </div>
         <div className="flex flex-col gap-1">
-          <Label htmlFor="phone-number">Phone Number</Label>
-          <Input
-            type="text"
+          <Label htmlFor="email-address">
+            Email Address
+            <ErrorMessage error={errors.email && errors.email.message} />
+          </Label>
+          <Controller
+            name="email"
+            control={control}
+            rules={{
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Please enter a valid email",
+              },
+              required: { value: true, message: "Email is required" },
+            }}
+            render={({ field }) => (
+              <Input
+                {...field}
+                placeholder="e.g. StephenKing@lorem.com"
+                hasError={errors.email}
+              />
+            )}
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <Label htmlFor="phone-number">
+            Phone Number
+            <ErrorMessage
+              error={errors["phone-number"] && errors["phone-number"].message}
+            />
+          </Label>
+
+          <Controller
             name="phone-number"
-            placeholder="e.g. +1 234 567 890"
+            control={control}
+            rules={{
+              required: "Phone number is required",
+            }}
+            render={({ field }) => (
+              <Input
+                {...field}
+                placeholder="e.g. +1 234 567 890"
+                hasError={errors["phone-number"]}
+                type="number"
+              />
+            )}
           />
         </div>
       </form>
